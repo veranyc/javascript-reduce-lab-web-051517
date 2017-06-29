5baab67e5951da11e10aca72b504336ecfffd0af
veranyc <veranyc@gmail.com>
2017-06-28 20:42:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +18,25 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+
+// amount of sentences mapped by word count.
+// example for first three lines >> {'7': 2, '9': 1}
+// 2 sentences with 7 words, and 1 sentence with 9 words
+// Create a new variable called wordCountMap that reduces the monologueLines to a map that resembles the output as shown above.
+
+let totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+
+let wordCountMap = monologueLines.reduce((object, line) => {
+  let wordCount = line.split(' ').length
+  debugger
+  if (object[wordCount]) {
+    object[wordCount] += 1
+  } else {
+    object[wordCount] = 1
+  }
+  return object
+}, {})
+
+wordCountMap()
